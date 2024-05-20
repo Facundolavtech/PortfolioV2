84cc7262c2ee3ff978cb91c7fe033a595ad62799
Facundolavtech <flavagnino8@gmail.com>
2024-05-19 21:40:15 -0300

    feat: extend tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,16 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#1A202C",
+      },
+    },
+  },
+  plugins: [],
 };
